51cb2645b3634d3963a7959fa9606b25dbe9593c
AureSor <103840115+AureSor@users.noreply.github.com>
2024-07-12 11:17:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,2 @@
-from snowflake.snowpark import Session
-import config
-
-# Configuration de la session Snowflake
-connection_parameters = {
-    'account': config.SNOWFLAKE_ACCOUNT,
-    'user': config.SNOWFLAKE_USER,
-    'password': config.SNOWFLAKE_PASSWORD,
-    'warehouse': config.SNOWFLAKE_WAREHOUSE,
-    'database': config.SNOWFLAKE_DATABASE,
-    'schema': config.SNOWFLAKE_SCHEMA,
-}
-
-# Créer la session Snowflake
-session = Session.builder.configs(connection_parameters).create()
-
-# Exécuter une requête de test
-result = session.sql("SELECT CURRENT_ACCOUNT(), CURRENT_REGION()").collect()
-print(result)
+import streamlit as st 
+st.write ("hello")
