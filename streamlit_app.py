65d87f99f8c2b67c5328a1ca8de16caf85632acb
AureSor <103840115+AureSor@users.noreply.github.com>
2024-07-12 20:11:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,11 @@
 import streamlit as st
 import pandas as pd
 import random
+from st_snowauth import snowauth_session
 
+st.markdown("## This (and above) is always seen")
+session = snowauth_session()
+st.markdown("## This (and below) is only seen after authentication")
 # Données d'exemple
 data = {
     'Mois': ['Janvier', 'Février', 'Mars', 'Avril', 'Mai'],
